630cf24c628cc65d9390f861899bfd7232c4399d
M-H-12 <MadisonJH12@outlook.com>
2024-10-11 15:32:15 -0600

    Added unit tests to cover more scenarios.

--- src/tests/stores.test.ts
+++ src/tests/stores.test.ts
@@ -31,31 +31,7 @@ it('Scrolling down on the title screen results in the project intro screen', asy
   wrapper.unmount()
 })
 
-it('Scrolling twice quickly does not change the page twice.', async () => {
-  const wrapper = mount(App, { attachTo: document.body })
-
-  await flushPromises()
-
-  const event = new WheelEvent('wheel') as any
-  event.wheelDelta = -10
-
-  document.dispatchEvent(event)
-
-  event.wheelDelta = -10
-
-  document.dispatchEvent(event)
-
-  //Wait 3 seconds for the scroll animation to finish.
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
-  await flushPromises()
-
-  expect(wrapper.text().includes('Residence')).not.toBe(true)
-
-  wrapper.unmount()
-})
-
-it('Scrolling up on the project info screen results in the title screen', async () => {
+it('Scrolling up on the project intro screen results in the title screen', async () => {
   const globalVariables = useGlobalVariables()
 
   const { currentScreen } = storeToRefs(globalVariables)
@@ -106,7 +82,7 @@ it('Swiping up on the title screen (mobile) results in the project intro screen'
   wrapper.unmount()
 })
 
-it('Swiping down on the project info screen (mobile) results in the title screen', async () => {
+it('Swiping down on the project intro screen (mobile) results in the title screen', async () => {
   const globalVariables = useGlobalVariables()
 
   const { currentScreen } = storeToRefs(globalVariables)
@@ -133,6 +109,144 @@ it('Swiping down on the project info screen (mobile) results in the title screen
   await flushPromises()
 
   expect(wrapper.text().includes('Madison Hill')).toBe(true)
+
+  wrapper.unmount()
+})
+
+it('Scrolling down on the project intro screen results in the Residence intro screen', async () => {
+  const globalVariables = useGlobalVariables()
+
+  const { currentScreen } = storeToRefs(globalVariables)
+
+  currentScreen.value = ScreenType.PROJECTS_INTRO
+
+  const wrapper = mount(App, { attachTo: document.body })
+
+  await flushPromises()
+
+  const event = new WheelEvent('wheel') as any
+  event.wheelDelta = -10
+
+  document.dispatchEvent(event)
+
+  //Wait 3 seconds for the scroll animation to finish.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  await flushPromises()
+
+  expect(wrapper.text().includes('Residence')).toBe(true)
+
+  wrapper.unmount()
+})
+
+it('Scrolling up on the Residence intro screen results in the project intro screen', async () => {
+  const globalVariables = useGlobalVariables()
+
+  const { currentScreen } = storeToRefs(globalVariables)
+
+  currentScreen.value = ScreenType.RESIDENCE_INTRO
+
+  const wrapper = mount(App, { attachTo: document.body })
+
+  await flushPromises()
+
+  const event = new WheelEvent('wheel') as any
+  event.wheelDelta = 10
+
+  document.dispatchEvent(event)
+
+  //Wait 3 seconds for the scroll animation to finish.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  await flushPromises()
+
+  expect(wrapper.text().includes('About')).toBe(true)
+
+  wrapper.unmount()
+})
+
+it('Swiping up on the project intro screen (mobile) results in the Residence intro screen', async () => {
+  const globalVariables = useGlobalVariables()
+
+  const { currentScreen } = storeToRefs(globalVariables)
+
+  currentScreen.value = ScreenType.PROJECTS_INTRO
+
+  const wrapper = mount(App, { attachTo: document.body })
+
+  await flushPromises()
+
+  const startEvent = new TouchEvent('touchstart', {
+    changedTouches: [{ screenY: 300 } as any]
+  }) as any
+
+  document.dispatchEvent(startEvent)
+
+  const endEvent = new TouchEvent('touchend', { changedTouches: [{ screenY: 200 } as any] }) as any
+
+  document.dispatchEvent(endEvent)
+
+  //Wait 3 seconds for the scroll animation to finish.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  await flushPromises()
+
+  expect(wrapper.text().includes('Residence')).toBe(true)
+
+  wrapper.unmount()
+})
+
+it('Swiping down on the Residence intro screen (mobile) results in the project intro screen', async () => {
+  const globalVariables = useGlobalVariables()
+
+  const { currentScreen } = storeToRefs(globalVariables)
+
+  currentScreen.value = ScreenType.RESIDENCE_INTRO
+
+  const wrapper = mount(App, { attachTo: document.body })
+
+  await flushPromises()
+
+  const startEvent = new TouchEvent('touchstart', {
+    changedTouches: [{ screenY: 200 } as any]
+  }) as any
+
+  document.dispatchEvent(startEvent)
+
+  const endEvent = new TouchEvent('touchend', { changedTouches: [{ screenY: 300 } as any] }) as any
+
+  document.dispatchEvent(endEvent)
+
+  //Wait 3 seconds for the scroll animation to finish.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  await flushPromises()
+
+  expect(wrapper.text().includes('About')).toBe(true)
+
+  wrapper.unmount()
+})
+
+it('Scrolling twice quickly does not change the page twice.', async () => {
+  const wrapper = mount(App, { attachTo: document.body })
+
+  await flushPromises()
+
+  const event = new WheelEvent('wheel') as any
+  event.wheelDelta = -10
+
+  document.dispatchEvent(event)
+
+  event.wheelDelta = -10
+
+  document.dispatchEvent(event)
+
+  //Wait 3 seconds for the scroll animation to finish.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  await flushPromises()
+
+  expect(wrapper.text().includes('Residence')).not.toBe(true)
 
   wrapper.unmount()
 })
