ba7514206c2c2be44421a3621675a4001021d267
M-H-12 <MadisonJH12@outlook.com>
2024-10-21 17:15:34 -0600

    Added content to residence intro page.

--- src/view/Residence/ResidenceIntro.vue
+++ src/view/Residence/ResidenceIntro.vue
@@ -28,8 +28,35 @@ onUnmounted(() => {
 <template>
   <div :class="$style.mainSegment">
     <br />
-    <div class="text-box-left">
-      <div class="section-text">Residence</div>
+    <div>
+      <div :class="$style.titleContainer">
+        <div class="section-text">Residence</div>
+      </div>
+      <div :class="$style.bottomSegment">
+        <div :class="$style.bodyText">
+          Residence is a passion project of mine that began several months ago. I started on this
+          project as a way to expand my skills with Vue.js, as well as to take on a project which
+          would have unique variety of programming challenges.
+          <br />
+          <br />
+          <div :class="$style.lowerContent">
+            <div :class="$style.textLower">
+              Like many people, my first introduction to programming came in my early teens through
+              video games. I was fascinated by how they worked, and hoped to create one of my own
+              one day. Thinking back to this memory, I started designing this project, and gave it
+              the working title of "Residence".
+              <br />
+              <br />
+              While the project is still very much in progress, it's come a long way from the
+              original idea, and has done more than just expand my programming knowledge and
+              expertise.
+            </div>
+            <div :class="$style.imageContainer">
+              <img :class="$style.image" src="@/assets/residenceIntro/titleImage.png" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
